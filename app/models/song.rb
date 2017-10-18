3c0a53a4ce431195010fd64b1c15e28befaa3edf
frankfil <frank.filius@gmail.com>
2017-10-18 16:46:48 +0200

    Change magic number into constant

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,9 @@
 class Song < ApplicationRecord
+  YEAR = Date.today.year
+  RELEASE_YEAR = 1300..YEAR
   belongs_to :artist
 
   validates :name, presence: true
   validates :album, presence: true
-  validates_inclusion_of :release_year, in: 1..2017
+  validates_inclusion_of :release_year, in: RELEASE_YEAR
 end
