437fb298d481b05aaf6c828e9b796255f0619b18
frankfil <frank.filius@gmail.com>
2017-10-18 16:50:51 +0200

    Set before_action in Songscontroller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,11 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+
   def new
-    @artist = Artist.find(params[:artist_id])
     @song = @artist.songs.build
   end
 
   def create
-    @artist = Artist.find(params[:artist_id])
     @song = @artist.songs.create(song_params)
     if @song.save
       redirect_to @artist, notice: "song created"
@@ -15,14 +15,17 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    artist = Artist.find(params[:artist_id])
-    song = artist.songs.find(params[:id])
+    song = @artist.songs.find(params[:id])
     song.destroy
 
-    redirect_to artist
+    redirect_to @artist
   end
 
   private
+
+  def set_artist
+    @artist = Artist.find(params[:artist_id])
+  end
 
   def song_params
     params.require(:song).permit(:name, :album, :release_year)
