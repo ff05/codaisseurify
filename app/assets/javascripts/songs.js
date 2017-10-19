8898447978f59325fb44ea77492f9a29bc671011
frankfil <frank.filius@gmail.com>
2017-10-19 22:55:10 +0200

    Add Songs with Ajax

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,28 +1,73 @@
-function createSong(name, album, year) {
-
-  var newTodo = { title: title, completed: false };
+function addSong(name, album, year) {
+  var newSong = {name: name, album: album, release_year: year}
 
   $.ajax({
     type: "POST",
     url: window.location.pathname + "/songs.json",
     data: JSON.stringify({
-        song: newSong
+      song: newSong
     }),
     contentType: "application/json",
-    dataType: "json"
-  });
+    dataType: "json"})
+    .done(function(data) {
+      var $delBtn = $('#del-button').clone();
+
+      var tableRow = $('<tr></tr>')
+       .append($('<td>').append($('<p>')).html(name))
+       .append($('<td>').append($('<p>')).html(album))
+       .append($('<td>').append($('<p>')).html(year))
+       .append($('<td>').append($delBtn));
+
+      $('#table').find($('tr')).first().after(tableRow);
+    })
+    .fail(function(error) {
+      console.log(error);
+
+      for (var key in error.responseJSON.errors ){
+        showError(error.responseJSON.errors[key]);
+      };
+
+    });
 }
 
-function submitSong(event){
+function deleteSong() {
+
+}
+
+function showError(message) {
+  var errorHelpBlock = $('<span class="help-block"></span>')
+    .attr('id', 'error_message')
+    .text(message);
+
+  $("#table").append(errorHelpBlock);
+}
+
+function saveSong(event){
   event.preventDefault();
+  resetErrors();
+  var songName = $('#song-name');
+  var songAlbum = $('#song-album');
+  var songYear = $('#song-year');
 
-  var name = $('#song-name').val();
-  var album = $('#song-album').val();
-  var year = $('#song-year').val();
-
-  createSong(name, album, year);
+  addSong(songName.val(), songAlbum.val(), songYear.val());
+  clearVals(songName, songAlbum, songYear);
+  // addSong($songName.val(), $songAlbum.val(), $songYear.val())
 }
 
+function clearVals(name, album, year) {
+  name.val(null);
+  album.val(null);
+  year.val(null);
+}
+
+function resetErrors() {
+  $("#error_message").remove();
+  $("#formgroup-title").removeClass("has-error");
+}
+
+$(document).ready(function() {
+  $('#add-button').bind('click', saveSong );
+})
 // 1. Create form in artists#show
 // 2. get values and add to table
 // 3. add as row in songs table
